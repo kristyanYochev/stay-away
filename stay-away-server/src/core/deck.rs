b1f75c2e3122f4a509f45864789c5107666f6c1e
Kristiyan Yochev <kris.yochev@gmail.com>
2023-02-23 19:12:02 +0000

    Add card counts for 4 players

--- stay-away-server/src/core/deck.rs
+++ stay-away-server/src/core/deck.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 pub struct Deck(HashMap<CardKind, u8>);
 
 /// Represents a single kind of card (i.e. Flamethrower)
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 pub enum CardKind {
     // Role cards
     TheThing,
@@ -85,7 +85,44 @@ impl Deck {
 }
 
 impl Deck {
-    fn player_count_to_card_counts(player_count: u8) -> HashMap<CardKind, u8> {
-        todo!("Hard-code the hashmaps")
+    fn player_count_to_card_counts(player_count: u8) -> Option<HashMap<CardKind, u8>> {
+        use CardKind::*;
+
+        match player_count {
+            4 => Some(HashMap::from([
+                (TheThing, 1),
+                (Infected, 8),
+                (Flamethrower, 2),
+                (Analysis, 0),
+                (Axe, 1),
+                (Suspicious, 4),
+                (Whiskey, 1),
+                (Resolute, 2),
+                (WatchYourBack, 1),
+                (OutOfHere, 2),
+                (YoudBetterRun, 2),
+                (Seduction, 2),
+                (Scary, 0),
+                (ImComfortable, 1),
+                (NoThanks, 1),
+                (Missed, 1),
+                (NoBBQ, 1),
+                (Quarantine, 0),
+                (BarredDoor, 1),
+                (RottenRopes, 0),
+                (OneTwo, 0),
+                (ThreeFour, 1),
+                (SoThisIsTheParty, 0),
+                (OutOfHere, 0),
+                (Forgetful, 1),
+                (RoundAndRound, 1),
+                (CantWeBeFriends, 0),
+                (BlindDate, 4),
+                (Ooops, 0),
+                (BetweenUs, 0),
+                (Revelations, 0),
+            ])),
+            _ => None,
+        }
     }
 }
